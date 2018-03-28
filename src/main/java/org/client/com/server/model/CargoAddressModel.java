fb97afde4d32f826300bcc1a805800a8e153da28
xuesemofa@126.com <xuesemofa@126.com>
2018-03-28 14:06:38 +0800

    调整model位置

--- src/main/java/org/client/com/server/model/CargoAddressModel.java
+++ src/main/java/org/client/com/server/model/CargoAddressModel.java
@@ -1,12 +1,5 @@
 package org.client.com.server.model;
 
-import org.client.com.cargoaddress.model.ADD;
-import org.client.com.cargoaddress.model.UPDATE;
-import org.hibernate.validator.constraints.NotBlank;
-
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 /**
  * @author ld
  * @name 收货地址
@@ -15,17 +8,10 @@ import javax.validation.constraints.Size;
  */
 public class CargoAddressModel {
 
-    @NotBlank(message = "主键丢失", groups = UPDATE.class)
     private String uuid;
-    @NotBlank(message = "用户标识丢失", groups = {ADD.class, UPDATE.class})
     private String accId;
-    @Size(max = 100, message = "收货地址限制长度为100位", groups = {ADD.class, UPDATE.class})
-    @Pattern(regexp = "^[^&#<>'\\\"\\\\|+%\\\\\\\\]*$", message = "常驻地址不能包含特殊符号",
-            groups = {ADD.class, UPDATE.class})
     private String address;
     //    是否为默认收货地址
-    @Size(max = 1, message = "默认地址选择错误", groups = {ADD.class, UPDATE.class})
-    @Pattern(regexp = "^[Y,N]$", message = "默认地址选择错误", groups = {ADD.class, UPDATE.class})
     private String defaults;
 
     public String getUuid() {
